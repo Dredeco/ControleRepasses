9ddf8836b11ae3c75f82a470be04c5092688b676
dredeco <andrefersouza@gmail.com>
2023-11-20 07:37:49 -0300

    fix: changed function export

--- src/app/Dashboard/[incidentNumber]/page.tsx
+++ src/app/Dashboard/[incidentNumber]/page.tsx
@@ -21,7 +21,7 @@ export async function generateStaticParams() {
     }))
 }
 
-export const IncidentInfo = ({ params }: any) => {
+const IncidentInfo = ({ params }: any) => {
 
   return (
     <RegisterFormSupervisor incidentNumber={params.incidentNumber} />
