290d0f702fad4142950b2fc5efd1ad5ff2d08906
dredeco <andrefersouza@gmail.com>
2023-11-20 08:16:13 -0300

    fix: changed incident page generation and form

--- src/app/Dashboard/[incidentNumber]/page.tsx
+++ src/app/Dashboard/[incidentNumber]/page.tsx
@@ -1,28 +1,19 @@
 import { getRegisterByNumber, getRegisters } from '@/api/RegisterService'
 import RegisterFormSupervisor from '@/components/RegisterFormSupervisor'
-import React from 'react'
+import React, { useEffect } from 'react'
+
 
 export async function generateStaticParams() {
-    const incidents = await getRegisters()
-   
-    return incidents.map((incident: IRegister) => ({
-        incidentNumber: incident.number,
-        number: incident.number,
-        task: incident.task,
-        sctask: incident.sctask,
-        date: incident.date,
-        user: incident.user,
-        supervisor: incident.supervisor,
-        classification: incident.classification,
-        system: incident.system,
-        fixProc: incident.fixProc,
-        observations: incident.observations,
-        supervisorObservations: incident.supervisorObservations
-    }))
+  const incidents = await getRegisters()
+  
+  return incidents.map((incident: IRegister) => ({
+    incidentNumber: incident.number,
+  }))
 }
 
 const IncidentInfo = ({ params }: any) => {
-
+  generateStaticParams()
+  
   return (
     <RegisterFormSupervisor incidentNumber={params.incidentNumber} />
   )
