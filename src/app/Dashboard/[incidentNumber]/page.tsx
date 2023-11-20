945a0102b3e3db6920270f572c32017cbca036dd
dredeco <andrefersouza@gmail.com>
2023-11-20 09:21:51 -0300

    fix: added options to route segment

--- src/app/Dashboard/[incidentNumber]/page.tsx
+++ src/app/Dashboard/[incidentNumber]/page.tsx
@@ -2,9 +2,9 @@ import { getRegisterByNumber, getRegisters } from '@/api/RegisterService'
 import RegisterFormSupervisor from '@/components/RegisterFormSupervisor'
 import React, { useEffect } from 'react'
 
-export const dynamicParams = true
+export const dynamicParams = 'force-dynamic'
 export const dynamic = 'auto'
-export const revalidate = false
+export const revalidate = 0
 
 export async function generateStaticParams() {
   const incidents = await getRegisters()
